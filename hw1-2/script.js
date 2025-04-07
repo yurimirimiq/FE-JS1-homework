f29b54ad55d8b05bd5e4851a6f4567aa3d51f1e1
강유림 <yurimirimiq@gmail.com>
2025-04-07 20:42:40 +0900

    1-2

--- hw1-2/script.js
+++ hw1-2/script.js
@@ -3,11 +3,9 @@ document.querySelector(".sliding").addEventListener("click", function () {
     const menu = document.querySelector(".menu");
     const openIcon = document.querySelector(".open");
     const closeIcon = document.querySelector(".close");
-  
-    // 메뉴 토글
+
     menu.classList.toggle("active");
   
-    // 아이콘 상태 토글
     if (menu.classList.contains("active")) {
       openIcon.style.display = "inline";
       closeIcon.style.display = "none";
